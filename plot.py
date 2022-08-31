545f3e89f4828d9b433430827bcffc792c3084a8
guandachen <74637081+guandachen@users.noreply.github.com>
2022-08-31 21:00:39 +0800

    update plot.py file

--- plot.py
+++ plot.py
@@ -24,8 +24,11 @@ para = np.zeros((2,100))
 epochs = 100
 times = 10
 nSample = 3000
+param_gen = []
+param_norm = []
+param_lap = []
 for time in range(times):
-    with open('gradient6-'+str(time)+'.pickle', 'rb') as f:
+    with open('gradient_17000nodes'+str(time)+'.pickle', 'rb') as f:
         gradient = pickle.load(f)
     for epoch in range(epochs):
         data = np.array(gradient[epoch])
@@ -36,33 +39,56 @@ for time in range(times):
         data_n = rv_x_new/rv_x_new.std()
         # generate the fit distribution to calculate the wasserstein_distance
         [mean_fit,std_fit] = norm.fit(data_n, floc=0)  # estimate norm distribution
-        distance[0, epoch] += W(2,norm.ppf(np.linspace(0,1,data_n.size),mean_fit, std_fit),data_n)
-        a = norm.ppf(np.linspace(0,1,data_n.size),mean_fit, std_fit)
-        distance[3, epoch] += energy_distance(a[1:a.size-1],data_n[1:data_n.size-1])
-        # distance_norm.append(distance1)
-       
+        param_norm.append(std_fit)
+        min_val = float("inf")
+        for i0 in param_norm:
+            temp = W(2,norm.ppf(np.linspace(0,1,data_n.size),0, i0),data_n)
+            min_val = min(temp, min_val)
+        distance[0, epoch] += min_val
+        #------#
+        in_val = float("inf")
+        for i0 in param_norm:
+            a = norm.ppf(np.linspace(0,1,data_n.size),0, i0)
+            temp = energy_distance(a[1:a.size-1],data_n[1:data_n.size-1])
+            min_val = min(temp, min_val)
+        distance[3, epoch] += min_val
+        ##
         [arg1,arg2] = laplace.fit(data_n, floc=0)  # estimate laplace distribution
-        distance[1, epoch] += W(2,laplace.ppf(np.linspace(0,1,data_n.size), arg1, arg2),data_n)
-        b = laplace.ppf(np.linspace(0,1,data_n.size), arg1, arg2)
-        distance[4, epoch] += energy_distance(b[1:b.size-1],data_n[1:data_n.size-1])
-        # distance_laplace.append(distance2)
+        param_lap.append(arg2)
+        in_val = float("inf")
+        for i0 in param_lap:
+            a = laplace.ppf(np.linspace(0,1,data_n.size),0, i0)
+            temp = energy_distance(a[1:a.size-1],data_n[1:data_n.size-1])
+            min_val = min(temp, min_val)
+        distance[4, epoch] += min_val
+        
+        min_val = float("inf")
+        for i0 in param_lap:
+            temp = W(2,laplace.ppf(np.linspace(0,1,data_n.size),0, i0),data_n)
+            min_val = min(temp, min_val)
+        distance[1, epoch] += min_val
+
        
         [arg4, arg5, arg6]= gennorm.fit(data_n, floc=0)  # estimate gennorm distribution
-        distance[2, epoch] += W(2,gennorm.ppf(np.linspace(0,1,data_n.size), beta=arg4, loc=arg5,scale= arg6),data_n)
-        c = gennorm.ppf(np.linspace(0,1,data_n.size), beta=arg4, loc=arg5,scale= arg6)
-        distance[5, epoch] += energy_distance(c[1:c.size-1],data_n[1:data_n.size-1])
-        # alpha_est = calculate_alpha(arg4, 1)
-        # print("Alpha:", arg6)
-        # print('Estimated', alpha_est)
-        # print('Error', arg6 - alpha_est)
-       
-        # distance_gennorm.append(distance3)
-        para[0,epoch] += arg4
-        para[1,epoch] += arg6*std_val
+        param_gen.append([arg4,arg6])
+        min_val = float("inf")
+        for i0,i1 in param_gen:
+            temp = W(2,gennorm.ppf(np.linspace(0,1,data_n.size), beta=i0, loc=0,scale=i1),data_n)
+            min_val = min(temp, min_val)
+
+        distance[2, epoch] += min_val
+        min_val = float("inf")
+        for i0,i1 in param_gen:
+            c = gennorm.ppf(np.linspace(0,1,data_n.size), beta=i0, loc=0,scale=i1)
+            temp = energy_distance(c[1:c.size-1],data_n[1:data_n.size-1])
+            min_val = min(temp, min_val)
+        distance[5, epoch] += min_val
+
+        para[0,epoch] += arg4 # after normalize the data 
+        para[1,epoch] += arg6*std_val # true alpha value
+    param_gen = []
 distance_mean = distance/times
 para_mean = para/times
-
-sum_beta = 0
 
 
 plt.figure(1)
